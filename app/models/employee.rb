e21b8880c3ff61a5762d5b7a18f9f781dfd92fef
Artem <artyezersky@gmail.com>
2020-03-20 07:28:37 +0200

    Style

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,8 +1,7 @@
 class Employee < ApplicationRecord
   belongs_to :department
-
+  validates :name, presence: true, length: { minimum: 3 }
   def self.search(search)
-    where("LOWER(name) LIKE ?", "%#{search.downcase}%")
-
+    where("LOWER(name) LIKE ?", "#{search.downcase}%")
   end
 end
